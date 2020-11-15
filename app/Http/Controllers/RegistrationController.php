0af3c5d7532f50f7fd2f68712451e8aa4e5b3216
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-15 14:51:46 +0530

    feat: implement method to register user

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -10,4 +10,17 @@ class RegistrationController extends Controller
     {
         return view('register');
     }
+
+    public function register(Request $request)
+    {
+        $validator = $request->validate([
+          'name'      => 'required|min:1',
+          'email'     => 'required',
+          'password'  => 'required|min:6'
+        ]);
+
+        \App\User::create($validator);
+
+        return redirect('/login');
+     }
 }
