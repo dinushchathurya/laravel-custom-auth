11be935207e5729d43cf6e8070b22f2b5d4d3278
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-15 16:09:15 +0530

    feat: create register user traits

---
+++ app/Traits/RegisterUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Traits;
+
+trait RegisterUser
+{
+    public function registerUser($fields)
+    {
+        $user = \App\User::create([
+            'name'      => $fields->name,
+            'email'     => $fields->email,
+            'password'  => $fields->password
+        ]);
+        return $user;
+    }
+}
